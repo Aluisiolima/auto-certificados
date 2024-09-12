82852b4c4be1a6027469f077c39ebd41960180c6
aluisio <aaluisio309@gmail.com>
2024-09-11 23:20:40 -0300

    create de class certificados para controle e aplicacao de poo nestes projeto

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ df = pd.read_excel(arquivo_excel, engine='openpyxl')
 
 # Escrever os dados no PDF
 for index, row in df.iterrows():
+    #criado obejeto Certificado
+    certificado = imgs.Certificado(nameAluno=dict(row)['Alunos'])
     # Criar um objeto FPDF
     pdf = FPDF()
     pdf.add_page()
 
-    image_path = imgs.nameAluno(dict(row)['Alunos'])  # Substitua pelo caminho da sua imagem
+    image_path = certificado.geraAluno()  # Substitua pelo caminho da sua imagem
     pdf.image(image_path, x=10, y=10, w=190)  # Ajuste a posição e o tamanho da imagem
 
-    pdf_output = f'pdfs/dados_excel{index}.pdf'
+    pdf_output = f'pdfs/cetificado-{dict(row)['Alunos']}.pdf'
     pdf.output(pdf_output)
     print(pdf_output)
     
--- mudandoImg.py
+++ mudandoImg.py
@@ -2,34 +2,54 @@ from PIL import Image, ImageDraw, ImageFont
 
 #texto = str(input("texto:"))
 
-def nameAluno(texto):
-    img = Image.open('certificado.png')
-    draw = ImageDraw.Draw(img)
+class Certificado:
+    def __init__(self,* ,nameAluno,cargaHoraria = None,dataInit = None,dataFinally = None,nameCurso = None,ano = None):
+        self.nameAluno = nameAluno
+        self.cargaHoraria = cargaHoraria
+        self.dataInit = dataInit
+        self.dataFinally = dataFinally
+        self.nameCurso = nameCurso   
+        self.ano = ano
+        self.img = Image.open('certificado.png')
+        self.draw = draw = ImageDraw.Draw(self.img)
 
-    font = ImageFont.truetype("fontes/GreatVibes-Regular.ttf", 150)
+       
+    def geraAluno(self):
+        draw = self.draw
+        nome = self.nameAluno
+        img = self.img
 
-    quadrado = [(627, 1184),(2872, 1434)]  
+        font = ImageFont.truetype("fontes/GreatVibes-Regular.ttf", 150)
 
-    draw.rectangle(quadrado,width=0,fill="#fbfbfb")
-    cor = (0,0,0) 
+        quadrado = [(627, 1184),(2872, 1434)]  
 
-
-    bbox = draw.textbbox((0, 0), texto, font=font)
-    text_width = bbox[2] - bbox[0]
-    text_height = bbox[3] - bbox[1]
-
-    # Calculando as coordenadas para centralizar o texto
-    x = (quadrado[0][0] + quadrado[1][0] - text_width) // 2
-    y = (quadrado[0][1] + quadrado[1][1] - text_height) // 2
-
-    draw.text((x,y), texto.capitalize(), font=font, fill=cor)
-    # Salvando a imagem com o texto
-    img_path = f'certificados/imagem_com_texto_{texto}.png'
-    img.save(img_path)
-    return img_path
+        draw.rectangle(quadrado,width=0,fill="#fbfbfb")
+        cor = (0,0,0) 
 
 
+        bbox = draw.textbbox((0, 0), nome, font=font)
+        text_width = bbox[2] - bbox[0]
+        text_height = bbox[3] - bbox[1]
+
+        # Calculando as coordenadas para centralizar o texto
+        x = (quadrado[0][0] + quadrado[1][0] - text_width) // 2
+        y = (quadrado[0][1] + quadrado[1][1] - text_height) // 2
+
+        draw.text((x,y), nome.capitalize(), font=font, fill=cor)
+        # Salvando a imagem com o texto
+        img_path = f'certificados/imagem_com_texto_{nome}.png'
+        img.save(img_path)
+        return img_path
 
 
+    
+    
+
+
+
+
+# def cargaHoraria(cargaHoraria){
+
+# }
 
 
